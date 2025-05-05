3a0eb049a0ecaf9eeca82c7ead585a7669bc0f75
tanisha <tanishasingh0302@gmail.com>
2025-05-05 14:43:48 +0530

    insertion

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const list = document.querySelector(".navlist");
+const hamburger = document.querySelector(".fa-bars");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("fa-x");
+    list.classList.toggle("navlist-active");
+})
+
+
+const list = document.querySelector(".navlist");
+const hamburger = document.querySelector(".fa-bars");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("fa-x");
+    list.classList.toggle("navlist-active");
+})
